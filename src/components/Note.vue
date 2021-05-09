a5b47dde4c179d14fec1a296e7b6400871de2083
Oscar Garza <ogarza2881@gmail.com>
2021-05-08 21:09:41 -0500

    Added Notes Design

---
+++ src/components/Note.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="bg-yellow-400 text-yellow-800 rounded text-center p-2 space-y-2">
+        <h3 class="text-2xl font-thin tracking-wide">Note Title</h3>
+        <p class="text-sm font-medium text-yellow-900">Lorem ipsum dolar sit mamet consecedlkj adlkjsadflksdjf . aslfkjsl;dfj . asldfkjasdlfjsadflskjf. asldf;kjsadf aslfj a;lsdkfj a;sldfjdsalf.</p>
+    <div class="flex justify-end space-x-2">
+        <button class="hover:text-green-800"><mdi:playlist-edit/></button>
+        <button class="hover:text-red-800"><raphael:trash/></button>
+    </div>
+ </div>
+</template>
